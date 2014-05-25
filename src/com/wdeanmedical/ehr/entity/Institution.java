70c832362f9823df6457cc521c5511eadc419196
Nicholas Sophinos <nick@wdeanmedical.com>
2014-05-25 14:55:26 -0400

    Institution and Facility entities added.

---
+++ src/com/wdeanmedical/ehr/entity/Institution.java
@@ -0,0 +1,75 @@
+/*
+ * WDean Medical is distributed under the
+ * GNU Lesser General Public License (GNU LGPL).
+ * For details see: http://www.wdeanmedical.com
+ * copyright 2013-2014 WDean Medical
+ */
+ 
+package com.wdeanmedical.ehr.entity;
+
+import java.io.Serializable;
+import java.util.Date;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "institution")
+public class Institution extends BaseEntity implements Serializable {
+
+  private String primaryPhone;
+  private String name;
+  private String secondaryPhone;
+  private String streetAddress1;
+  private String streetAddress2;
+  private String city;
+  private USState usState;
+  private String postalCode;
+  private Country country;
+
+  public Institution() {
+  }
+
+
+  @Column(name = "name")
+  public String getName() { return name; }
+  public void setName(String name) { this.name = name; }
+
+  @Column(name = "postal_code")
+  public String getPostalCode() { return postalCode; }
+  public void setPostalCode(String postalCode) { this.postalCode = postalCode; }
+
+  @Column(name = "primary_phone")
+  public String getPrimaryPhone() { return primaryPhone; }
+  public void setPrimaryPhone(String primaryPhone) { this.primaryPhone = primaryPhone; }
+
+  @Column(name = "secondary_phone")
+  public String getSecondaryPhone() { return secondaryPhone; }
+  public void setSecondaryPhone(String secondaryPhone) { this.secondaryPhone = secondaryPhone; }
+
+  @Column(name = "street_address1")
+  public String getStreetAddress1() { return streetAddress1; }
+  public void setStreetAddress1(String streetAddress1) { this.streetAddress1 = streetAddress1; }
+
+  @Column(name = "street_address2")
+  public String getStreetAddress2() { return streetAddress2; }
+  public void setStreetAddress2(String streetAddress2) { this.streetAddress2 = streetAddress2; }
+
+  @Column(name = "city")
+  public String getCity() { return city; }
+  public void setCity(String city) { this.city = city; }
+
+  @JoinColumn(name = "us_state", referencedColumnName = "id")
+  @ManyToOne(optional = true)
+  public USState getUsState() { return usState; }
+  public void setUsState(USState usState) { this.usState = usState; }
+
+  @JoinColumn(name = "country", referencedColumnName = "id")
+  @ManyToOne(optional = true)
+  public Country getCountry() { return country; }
+  public void setCountry(Country country) { this.country = country; }
+
+}
