b2866f0588c5fce48897f4b1b72a51a6aea66b83
bobbydaniel <bob.daniel@gmail.com>
2014-09-25 18:36:29 -0700

    Cache all screen selectors.

--- web/js/view_stacks.js
+++ web/js/view_stacks.js
@@ -4,48 +4,58 @@
  * For details see: http://www.wdeanmedical.com
  * copyright 2013-2014 WDean Medical
  */
- 
- var hide_element_list_cache = $(['#signin-screen','#dashboard-screen','#patient-chart-screen','#schedule-screen',
+
+var hide_element_list_cache = $(['#signin-screen','#dashboard-screen','#patient-chart-screen','#schedule-screen',
   '#messages-screen','#letters-screen','#reports-screen','#reports-list','#reports-view','#patient-encounters-screen',
   '#user-admin-screen','#app-dropdown-logout','#app-dropdown-settings','#app-dropdown-signin',
   '#main-navigation','#messages-view','#messages-inbox'].join(','));
- 
+  
+var signin_cache = $(['#signin-screen','#app-dropdown-signin'].join(','));
+var dashboard_cache = $(['#dashboard-screen','#app-dropdown-settings','#app-dropdown-logout','#main-navigation'].join(','));
+var patient_chart_cache = $(['#patient-chart-screen','#app-dropdown-settings','#app-dropdown-logout','#main-navigation'].join(','));
+var schedule_cache = $(['#schedule-screen','#app-dropdown-settings','#app-dropdown-logout','#main-navigation'].join(','));
+var messages_cache = $(['#messages-screen','#app-dropdown-settings','#app-dropdown-logout','#main-navigation','#messages-inbox'].join(','));
+var letters_cache = $(['#letters-screen','#app-dropdown-settings','#app-dropdown-logout','#main-navigation'].join(','));
+var reports_cache = $(['#reports-screen','#reports-list','#app-dropdown-settings','#app-dropdown-logout','#main-navigation'].join(','));
+var patient_encounters_cache = $(['#patient-encounters-screen','#app-dropdown-settings','#app-dropdown-logout','#main-navigation'].join(','));
+var user_admin_cache = $(['#user-admin-screen','#app-dropdown-settings','#app-dropdown-logout','#main-navigation'].join(','));
+
 function showScreen(page, screen) {
   $('#app-page-name').html(page);
   hide_element_list_cache.css({display: "none"});
-  $(screen.join(',')).css({display: "block"});
+  screen.css({display: "block"});
 }
 
 function app_viewStack(screen, doScroll) {
   app_previousScreen = app_currentScreen;
   switch (screen) {
     case 'signin-screen':
-      showScreen('', ['#signin-screen','#app-dropdown-signin']);
+      showScreen('', signin_cache);
     break;
     case 'dashboard-screen':
-      showScreen('Dashboard', ['#dashboard-screen','#app-dropdown-settings','#app-dropdown-logout','#main-navigation']);
+      showScreen('Dashboard', dashboard_cache);
       getClinicianDashboard();
     break;
     case 'patient-chart-screen':
-      showScreen('Patient Chart', ['#patient-chart-screen','#app-dropdown-settings','#app-dropdown-logout','#main-navigation']);
+      showScreen('Patient Chart', patient_chart_cache);
     break;
     case 'schedule-screen':
-      showScreen('Schedule', ['#schedule-screen','#app-dropdown-settings','#app-dropdown-logout','#main-navigation']);
+      showScreen('Schedule', schedule_cache);
     break;        
     case 'messages-screen':
-      showScreen('Messages', ['#messages-screen','#app-dropdown-settings','#app-dropdown-logout','#main-navigation','#messages-inbox']);
+      showScreen('Messages', messages_cache);
     break;
     case 'letters-screen':
-      showScreen('Letters', ['#letters-screen','#app-dropdown-settings','#app-dropdown-logout','#main-navigation']);
+      showScreen('Letters', letters_cache);
     break;
     case 'reports-screen':
-      showScreen('Reports', ['#reports-screen','#reports-list','#app-dropdown-settings','#app-dropdown-logout','#main-navigation']);
+      showScreen('Reports', reports_cache);
     break;
     case 'patient-encounters-screen':
-      showScreen('Patient Encounter', ['#patient-encounters-screen','#app-dropdown-settings','#app-dropdown-logout','#main-navigation']);
+      showScreen('Patient Encounter', patient_encounters_cache);
     break;
     case 'user-admin-screen':
-      showScreen('', ['#user-admin-screen','#app-dropdown-settings','#app-dropdown-logout','#main-navigation']);
+      showScreen('', user_admin_cache);
     break;
   }
   if (doScroll) {scroll(0,0);}
