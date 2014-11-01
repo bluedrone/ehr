6fd38f4b7ef727c37431d6f87a8dd0bd5a09ba52
rajeshjrl <rajeshjrl@gmail.com>
2014-10-31 20:53:27 -0400

    removed unnecessary imports.

--- src/com/wdeanmedical/ehr/entity/PatientFollowUp.java
+++ src/com/wdeanmedical/ehr/entity/PatientFollowUp.java
@@ -9,14 +9,10 @@ package com.wdeanmedical.ehr.entity;
 
 import java.io.Serializable;
 import java.util.Date;
-import java.util.List;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
-import javax.persistence.JoinColumn;
-import javax.persistence.ManyToOne;
 import javax.persistence.Table;
-import javax.persistence.Transient;
 
 @Entity
 @Table(name = "patient_follow_up")
